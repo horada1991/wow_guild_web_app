2c3e3a1ff76b9e0d1023b4c6e15a219860759478
Ádám Horváth <horada1991@gmail.com>
2016-10-10 17:50:48 +0200

    handle db class

--- controll/handle_tables.py
+++ controll/handle_tables.py
@@ -1,1 +1,12 @@
 from table_models import *
+
+
+class TableHandler:
+    def __init__(self, table):
+        self.table=table
+
+    def get_all_entry(self):
+        return self.table.select()
+
+    def save_entry(self, entry_dictionary):
+        self.table.create(**entry_dictionary)
